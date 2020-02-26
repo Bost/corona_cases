e63d109d18d6ac6f9c8bee108b2ec09ba36c8983
Bost <thebost@gmail.com>
2020-02-26 02:20:21 +0100

    Add new csv files

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject coronavirus
-  "1.3.1"
+  "1.3.2"
   :description "Telegram Chatbot for tracking coronavirus information"
   :url "http://corona-cases-bot.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
