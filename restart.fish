2d1505e3f9656dc209e1a5366a6b3445282af6b5
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-03-13 12:19:54 +0100

    restart.fish: --prod --test param recognition

--- restart.fish
+++ restart.fish
@@ -1,11 +1,30 @@
 #!/usr/bin/env fish
 
+# examples:
+# ./restart --prod
+# ./restart --test
+
 # set up environment
-# set --local envName corona-cases  # prod
-set --local envName hokuspokus    # test
+set botEnvs $botEnvs --test
+set botEnvs $botEnvs --prod
+
+set prmEnvName $argv[1]
+
+if test $prmEnvName = $botEnvs[1]
+    set --local envName hokuspokus
+else if test $prmEnvName = $botEnvs[2]
+    set --local envName corona-cases
+else
+    echo "ERROR: Unknown parameter:" $prmEnvName
+    echo "Possible values:" $botEnvs
+    exit 1
+end
 
 set --local APP $envName"-bot"
 set --local REMOTE "heroku-"$APP
 
-heroku ps:scale web=0 --app $APP; and \
-heroku ps:scale web=1 --app $APP
+set cmd heroku ps:scale web=0 --app $APP; and heroku ps:scale web=1 --app $APP
+
+echo $cmd
+eval $cmd
+
