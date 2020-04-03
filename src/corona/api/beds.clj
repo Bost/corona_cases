2c33e8625f05fca7ac8a3b9acbdcb9a82447ddb3
Dave Liepmann <dave.liepmann@gmail.com>
2020-04-03 10:41:38 +0200

    Clean up beds namespace
    * add TODOs so one could understand what it's supposed to do
    * nix crufty code
    * `:refer` > `:use`

--- src/corona/api/beds.clj
+++ src/corona/api/beds.clj
@@ -1,6 +1,6 @@
 (ns corona.api.beds
-  (:use [hickory.core])
-  (:require [hickory.select :as s]
+  (:require [hickory.core :refer [parse as-hickory]]
+            [hickory.select :as s]
             [clj-http.client :as client]
             [clojure.string :as string]))
 
@@ -9,39 +9,28 @@
 
 (def site (-> (client/get url) :body parse as-hickory))
 
-(defn h []
+(defn h ;; FIXME descriptive name
+  ;; TODO docstring / document return value -- what does this seq of numbers represent?
+  []
   (as-> (s/select (s/child
-                 #_(s/class "subCalender") ; sic
-                 #_(s/tag :table)
-                 #_(s/tag :tbody)
-                 (s/tag :tr)
-                 #_(s/id :raceDates)
-                 #_s/first-child
-                 #_(s/tag :b))
-                site) $
-      ;; (nth 0) ;; first ; table header
-      ;; (nth 1 );; second row
-      ;; (nth 2) ;; years
-      (drop 3 $)
-      #_(take 1 $)
-      #_(nth 3)
-      (mapv (fn [row]
-             (as-> (:content row) $
-               (filter (fn [hm]
-                         (let [content (:content hm)]
-                           (and (vector? content)
-                                (= 1 (count content))
-                                (string? (first content))
-                                ))) $)
-               (into [] $)
-               (nth $ 6) ; 7th column is the year 2017 - now every
-               (:content $)
-               (keep (fn [s]
+                   (s/tag :tr))
+                  site) $
+    (drop 3 $)
+    (mapv (fn [row]
+            (as-> (:content row) $
+              (filter (fn [hm]
+                        (let [content (:content hm)]
+                          (and (vector? content)
+                               (= 1 (count content))
+                               (string? (first content))
+                               ))) $)
+              (into [] $)
+              (nth $ 6) ; 7th column is the year 2017 - now every
+              (:content $)
+              (keep (fn [s]
                       (let [s-nr (subs s 0 (- (count s) 1))]
                         (if (empty? s-nr)
                           0
                           (read-string s-nr))))
-
-                     $)
-               )) $)
-      (reduce into [] $)))
+                    $))) $)
+    (reduce into [] $)))
