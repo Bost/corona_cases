92339b9b93c9e429d928067de848d62c53b9dc0b
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-04-13 16:52:20 +0200

    wip: proper country-code types

--- src/corona/defs.clj
+++ src/corona/defs.clj
@@ -5,12 +5,14 @@
 (def worldwide-3-country-code (-> worldwide-country-codes vals first))
 (def worldwide              "Worldwide")
 (def country-code-worldwide {worldwide-2-country-code worldwide})
+(def zz worldwide-2-country-code)
 
 (def default-country-codes   {"QQ" "QQQ"})
 (def default-2-country-code (-> default-country-codes keys first))
 (def default-3-country-code (-> default-country-codes vals first))
 (def others                 "Others")
 (def country-code-others    {default-2-country-code others})
+(def qq default-2-country-code)
 
 (def cruise-ship-2-country-code default-2-country-code)
 (def cruise-ship-3-country-code default-3-country-code)
