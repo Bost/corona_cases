8b4de0efd1ed26bb6d8093c50ca2963a618f7e84
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-06-24 17:13:51 +0200

    project.clj: remove exclusions

--- project.clj
+++ project.clj
@@ -21,41 +21,20 @@
    [compojure               "1.6.1"]
 
    ;; Ring Jetty adapter
-   [ring/ring-jetty-adapter "1.8.1"
-    :exclusions
-    [
-     ring/ring-core
-     ring/ring-codec
-     commons-fileupload
-     ]
-    ]
-
-   [cider/cider-nrepl "0.25.2"
-    ;; :exclusions [nrepl]
-    ]
+   [ring/ring-jetty-adapter "1.8.1"]
 
    ;; managing environment variables
    [environ                 "1.2.0"]
 
-   ;; JSON and JSON SMILE encoding
-   [cheshire "5.10.0"]  ;; it looks like the clj-http requires this dependency.
-                        ;; otherwise I get `namespace 'cheshire.factory' not
-                        ;; found`
-  / ;; for the get-json function
+   ;; JSON and JSON SMILE encoding - see also clj-http
+   [cheshire "5.10.0"]
+
+   ;; for the get-json function. Not having it cheshire as a dependency results
+   ;; in: `namespace 'cheshire.factory' not found`
    [clj-http                "3.10.1"]
 
    ;; Clojure interface for Telegram Bot API
-   [org.clojars.bost/morse  "0.0.0-157-0x8c5c"
-    :exclusions
-    [
-     org.clojure/spec.alpha
-     org.clojure/core.cache
-     com.fasterxml.jackson.core/jackson-core
-     org.clojure/tools.analyzer
-     org.clojure/tools.analyzer.jvm
-     org.clojure/core.memoize
-     ]
-    ]
+   [org.clojars.bost/morse  "0.0.0-157-0x8c5c"]
 
    [org.clojure/data.json   "1.0.0"]
    [clojure.java-time       "0.3.2"]
@@ -75,21 +54,14 @@
    ;; parse HTML into Clojure data structures - scrapping data from HTML tables
    [hickory "0.7.1"]
 
-   ;; for klipse
-   [org.clojure/clojurescript "1.10.773"
-    :exclusions
-    [
-     com.google.errorprone/error_prone_annotations
-     com.google.jsinterop/jsinterop-annotations
-     org.clojure/google-closure-library-third-party
-     com.google.javascript/closure-compiler-externs
-     com.google.protobuf/protobuf-java
-     ]
-    ]
+   ;; {{{ klipse stuff
+   [org.clojure/clojurescript "1.10.773"]
 
    ;; web repl
    [viebel/klipse "7.9.6"]
+   ;; }}}
    ]
+
   :min-lein-version "2.0.0"
   :uberjar-name "corona_cases-standalone.jar"
   :profiles {:production {:env {:production true}}})
