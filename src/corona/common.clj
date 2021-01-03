d9b4fdcbcca1851c4b22c83d389640ff60d90833
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-01-03 20:31:50 +0100

    Raise thresholds

--- src/corona/common.clj
+++ src/corona/common.clj
@@ -245,10 +245,10 @@
   :a ~ active cases i.e. ill"
   [
    {:idx  0 :kw :p                :threshold {:inc (int 1e6) :val (int 1e7)}}
-   {:idx  1 :kw :c                :threshold {:inc 50000     :val (int 1660e3)}}
-   {:idx  2 :kw :r :listing-idx 1 :threshold {:inc 10000     :val (int 1037e3)}}
-   {:idx  3 :kw :d :listing-idx 2 :threshold {:inc 1000      :val (int 50e3)}}
-   {:idx  4 :kw :a :listing-idx 0 :threshold {:inc 10000     :val (int 619e3)}}
+   {:idx  1 :kw :c                :threshold {:inc 50000     :val (int 1810e3)}}
+   {:idx  2 :kw :r :listing-idx 1 :threshold {:inc 10000     :val (int 1097e3)}}
+   {:idx  3 :kw :d :listing-idx 2 :threshold {:inc 1000      :val (int 51e3)}}
+   {:idx  4 :kw :a :listing-idx 0 :threshold {:inc 10000     :val (int 639e3)}}
    {:idx  5 :kw :a100k}
    {:idx  6 :kw :r100k}
    {:idx  7 :kw :d100k}
