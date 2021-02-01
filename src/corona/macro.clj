010e237c186091a1bcb6adbf8dbf02466ad55198
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-02-01 20:29:47 +0100

    wip: macros

--- src/corona/macro.clj
+++ src/corona/macro.clj
@@ -8,12 +8,12 @@
      (fn ~args (let [~'fun-id (str (quote ~name))]
                  ~@exprs))))
 
-(defn-fun-id f "f-docs" [x]
+#_(defn-fun-id f "f-docs" [x]
   {:pre [(number? x)]}
   (printf "[%s] hi\n" fun-id)
   (inc x))
 
-(defn-fun-id g "f-docs" [x]
+#_(defn-fun-id g "f-docs" [x]
   (printf "[%s] hi\n" fun-id)
   (inc (f x)))
 
@@ -29,3 +29,17 @@
 ;;   1)
 
 #_(defmacro debugf [s & exprs] `((quote ~'debugf) (str "[%s] " ~s) ~'fun-id ~@exprs))
+
+#_(defmacro defn-fun-id2
+  "Docstring is mandatory!"
+  [name docstr args & exprs]
+  `(def ~name
+     ~docstr
+     (fn ~args
+       (let [fun-id# (str (quote ~name))]
+         (fn debugf [s & exprs] `(taoensso.timbre/debugf (str "[%s] " ~s) fun-id# ~@exprs))
+         (fn infof  [s & exprs] `(taoensso.timbre/infof  (str "[%s] " ~s) fun-id# ~@exprs))
+         (fn warnf  [s & exprs] `(taoensso.timbre/warnf  (str "[%s] " ~s) fun-id# ~@exprs))
+         (fn errorf [s & exprs] `(taoensso.timbre/errorf (str "[%s] " ~s) fun-id# ~@exprs))
+         (fn fatalf [s & exprs] `(taoensso.timbre/fatalf (str "[%s] " ~s) fun-id# ~@exprs))
+         ~@exprs))))
