da49f1bcc6421a249c6bec42778575818f37a189
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-02-24 13:24:25 +0100

    Raise thresholds

--- src/corona/common.clj
+++ src/corona/common.clj
@@ -376,8 +376,8 @@ https://clojurians.zulipchat.com/#narrow/stream/151168-clojure/topic/hashmap.20a
   [{:idx  0 :kw :v                :threshold {:inc (int 1e6) :val (int 1e7)}}
    {:idx  1 :kw :p                :threshold {:inc (int 1e6) :val (int 1e7)}}
    {:idx  2 :kw :c                :threshold {:inc 50000     :val (int 2410e3)}}
-   {:idx  3 :kw :r :listing-idx 1 :threshold {:inc 10000     :val (int 1597e3)}}
-   {:idx  4 :kw :d :listing-idx 2 :threshold {:inc 1000      :val (int 68e3)}}
+   {:idx  3 :kw :r :listing-idx 1 :threshold {:inc 10000     :val (int 1617e3)}}
+   {:idx  4 :kw :d :listing-idx 2 :threshold {:inc 1000      :val (int 69e3)}}
    {:idx  5 :kw :a :listing-idx 0 :threshold {:inc 10000     :val (int 739e3)}}
    ;; TODO the order matters: it must be the same as in the info-message
    {:idx  6 :kw :v100k}
