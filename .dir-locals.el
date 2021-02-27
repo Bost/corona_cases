e14eb157d0421d0864ad688e9863a741aa4e1010
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-02-27 23:27:43 +0100

    TODO analyze heap dumps

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
-((clojure-mode . ((cider-preferred-build-tool . clojure-cli)
-                  ;; (cider-clojure-cli-parameters . "-J-Djdk.attach.allowAttachSelf")
-                  ;; -Xmx<size> - keep in sync with Procfile
-                  (cider-clojure-cli-global-options . "-J-Xmx400m -J-Djdk.attach.allowAttachSelf"))))
+((clojure-mode
+  .
+  ((cider-preferred-build-tool . clojure-cli)
+   ;; (cider-clojure-cli-parameters . "-J-Djdk.attach.allowAttachSelf")
+   ;; -Xmx<size> - keep in sync with Procfile
+   (cider-clojure-cli-global-options
+    .
+    "-J-Xmx400m -J-XX:+HeapDumpOnOutOfMemoryError -J-Djdk.attach.allowAttachSelf"))))
