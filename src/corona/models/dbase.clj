ba655c4c160e1c962c8b641e58233a348a8c8f40
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-02-27 21:58:07 +0100

    dbase: use prepate-statement for insert-chat!

--- src/corona/models/dbase.clj
+++ src/corona/models/dbase.clj
@@ -4,7 +4,7 @@
   (:require [next.jdbc :as jdbc]
             [environ.core :refer [env]]
             [taoensso.timbre :as timbre]
-            [corona.macro :refer [defn-fun-id debugf]]
+            [corona.macro :refer [defn-fun-id debugf errorf]]
             [corona.models.common :as mcom]
             [utils.core :as utc]))
 
@@ -17,30 +17,41 @@
         (fn [cmd] (jdbc/execute! connection [cmd id]))
         #_(reduce my-fn init-value (jdbc/plan connection [...]))
         (fn [cmd] (debugf "\n%s" cmd) cmd))
-       (format "select %s(select 1 from %s where id = ?)" (name exists-kw) (pr-str table))))))
+       (format "select %s(select 1 from %s where id = ?)"
+               (name exists-kw)
+               (pr-str table))))))
+
+(defn prepate-statement [table cols]
+  (format "insert into %s (%s) values (%s)"
+          (pr-str table)
+          ((comp
+            (fn [col] (utc/sjoin col ", "))
+            (partial map name))
+           cols)
+          ((comp
+            (fn [cols] (utc/sjoin cols ", "))
+            (partial map (fn [col]
+                           (cond
+                             (= :type col) "cast(? as chat_type)"
+                             (= :created_at col) "cast(? as timestamp(0))"
+                             :else "?"))))
+           cols)))
 
 (defn-fun-id insert-chat! "" [{:keys [id first_name username type] :as prm}]
   (with-open [connection (jdbc/get-connection mcom/datasource)]
-    (def table "chat")
-    (let [cols [:id :first_name :username :type]
-          row-vals
-          #_[(rand-nth [1111111111 2222222222 3333333333]) "Jim" "Beam" "private"]
-          [id first_name username type]]
+    (let [table "chat"
+          cols [:id :first_name :username :type :created_at]
+          prep-stmt (prepate-statement table cols)]
       ((comp
-        (fn [cmd] (jdbc/execute-one! connection [cmd]))
-        #_(reduce my-fn init-value (jdbc/plan connection [...]))
-        (fn [cmd] (timbre/debugf "\n%s" cmd)
-          cmd))
-       (format "insert into %s (%s) values (%s)"
-               (pr-str table)
-               ((comp
-                 (fn [col] (utc/sjoin col ", "))
-                 (partial map name))
-                cols)
-               ((comp
-                 (fn [col-vals] (utc/sjoin col-vals ", "))
-                 (partial map (fn [v] (if (string? v) (format "'%s'" v) v))))
-                row-vals))))))
+        (fn [res] (debugf "res: %s" res) res)
+        (fn [cmd] (try (jdbc/execute-one! connection cmd)
+                      (catch Exception e
+                        (errorf "Caught %s" e))))
+        (fn [cmd] (debugf "cmd %s" cmd) cmd)
+        (partial into [prep-stmt]))
+       #_[(rand-nth [1111111111 2222222222 3333333333])
+          "Jim" "Beam" "private" "now()"]
+       [id first_name username type "now()"]))))
 
 (comment
   (with-open [connection (jdbc/get-connection mcom/datasource)]
