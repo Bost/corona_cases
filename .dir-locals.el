4f949054109f13927b701aef0c6b3ba89e0350a7
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-05-23 16:54:05 +0200

    emacs ide: s-f5: v1.clj s-f6: telegram.clj s-f7: details.clj

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,15 @@
-((clojure-mode
+(
+ ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Rebinding.html
+ ;; see local-function-key-map
+ ;; `M-x local-set-key RET key cmd RET' seems not to be permanent
+ (nil . ((eval . (setq corona-home (format "%s/dec/corona_cases" (getenv "HOME"))))
+         (eval . (set-local-keymap (kbd "<s-f5>")
+                                   '(find-file (format "%s/src/corona/api/v1.clj" corona-home))))
+         (eval . (set-local-keymap (kbd "<s-f6>")
+                                   '(find-file (format "%s/src/corona/telegram.clj" corona-home))))
+         (eval . (set-local-keymap (kbd "<s-f7>")
+                                   '(find-file (format "%s/src/corona/msg/text/details.clj" corona-home))))))
+ (clojure-mode
   .
   ((cider-preferred-build-tool . clojure-cli)
    ;; (cider-clojure-cli-parameters . "-J-Djdk.attach.allowAttachSelf")
